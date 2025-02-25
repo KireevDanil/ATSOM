d12889773169ba1743a3353c8d49c30da97017ab
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2025-02-25 20:45:56 +0300

    Task 5

--- Lab2/task1_2.py
+++ Lab2/task1_2.py
@@ -1,35 +1,250 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0) 
+def task_1():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()  # Чтение текущего кадра
+        if not ret:  # Проверка на успешное чтение кадра
+            break
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Не удалось захватить изображение с камеры")
-        break
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow("Web HSV", hsv_frame)  # Отображение кадра
 
-    # Переводим изображение в формат HSV
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # Ожидание 20 мс и проверка нажатия клавиши 'Esc' (код 27)
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
 
-    lower_red1 = np.array([0, 40, 200])
-    upper_red1 = np.array([20, 180, 255])
+    # Освобождение ресурсов
+    cap.release()
+    cv2.destroyAllWindows()
+    cv2.waitKey(0)
 
-    lower_red2 = np.array([170, 40, 200])
-    upper_red2 = np.array([180, 180, 255])
+def task2_filtering():
+    # захват видео
+    cap = cv2.VideoCapture(0)
 
-    mask1 = cv2.inRange(hsv_frame, lower_red1, upper_red1)
-    mask2 = cv2.inRange(hsv_frame, lower_red2, upper_red2)
+    def nothing(args):
+        pass
 
-    # Комбинируем маски
-    mask = mask1 + mask2
-    # Применяем маску к оригинальному изображению
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow('Result', result)
+    # создаем окно для отображения результата и бегунки
+    cv2.namedWindow("setup",cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("setup", 300, 200)  # Изменяем размер окна управления
+    cv2.createTrackbar("minH", "setup", 119, 179, nothing)
+    cv2.createTrackbar("minS", "setup", 140, 255, nothing)
+    cv2.createTrackbar("minV", "setup", 3, 255, nothing)
+    cv2.createTrackbar("maxH", "setup", 137, 179, nothing)
+    cv2.createTrackbar("maxS", "setup", 255, 255, nothing)
+    cv2.createTrackbar("maxV", "setup", 255, 255, nothing)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# Освобождаем ресурсы
-cap.release()
-cv2.destroyAllWindows()
+
+    while True:
+        ret, frame = cap.read()  # Чтение текущего кадра
+        if not ret:  # Проверка на успешное чтение кадра
+            break
+
+        # собираем значения из бегунков в множества
+        minH = cv2.getTrackbarPos('minH', 'setup')
+        minS = cv2.getTrackbarPos('minS', 'setup')
+        minV = cv2.getTrackbarPos('minV', 'setup')
+        maxH = cv2.getTrackbarPos('maxH', 'setup')
+        maxS = cv2.getTrackbarPos('maxS', 'setup')
+        maxV = cv2.getTrackbarPos('maxV', 'setup')
+        min_p = (minH, minS, minV)
+        max_p = (maxH, maxS, maxV)
+
+
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+        hsv_frame[:, :, 0] = (hsv_frame[:, :, 0] + 128) % 0xFF # смещение красного цвета в центр
+        #чтобы не искать спектр мы его приведем в нужные значения самостоятельно
+
+        # применяем фильтр, делаем бинаризацию
+        mask = cv2.inRange(hsv_frame, min_p, max_p)
+
+
+        hsv_frame_filtered = cv2.bitwise_and(frame, frame, mask=mask)  # создание фильтра
+        cv2.imshow('Filtered Web Video', hsv_frame_filtered)
+
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+def task3_morph_transform():
+    # захват видео
+    cap = cv2.VideoCapture(0)
+
+    def nothing(args):
+        pass
+
+    # создаем окно для отображения результата и бегунки
+    cv2.namedWindow("setup",cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("setup", 300, 200)  # Изменяем размер окна управления
+    cv2.createTrackbar("minH", "setup", 119, 179, nothing)
+    cv2.createTrackbar("minS", "setup", 140, 255, nothing)
+    cv2.createTrackbar("minV", "setup", 3, 255, nothing)
+    cv2.createTrackbar("maxH", "setup", 137, 179, nothing)
+    cv2.createTrackbar("maxS", "setup", 255, 255, nothing)
+    cv2.createTrackbar("maxV", "setup", 255, 255, nothing)
+
+    while True:
+        ret, frame = cap.read()  # Чтение текущего кадра
+        if not ret:  # Проверка на успешное чтение кадра
+            break
+
+        # собираем значения из бегунков в множества
+        minH = cv2.getTrackbarPos('minH', 'setup')
+        minS = cv2.getTrackbarPos('minS', 'setup')
+        minV = cv2.getTrackbarPos('minV', 'setup')
+        maxH = cv2.getTrackbarPos('maxH', 'setup')
+        maxS = cv2.getTrackbarPos('maxS', 'setup')
+        maxV = cv2.getTrackbarPos('maxV', 'setup')
+        min_p = (minH, minS, minV)
+        max_p = (maxH, maxS, maxV)
+
+
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+        hsv_frame[:, :, 0] = (hsv_frame[:, :, 0] + 128) % 0xFF # смещение красного цвета в центр
+        #чтобы не искать спектр мы его приведем в нужные значения самостоятельно
+
+        # применяем фильтр, делаем бинаризацию
+        mask = cv2.inRange(hsv_frame, min_p, max_p)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5)))  # erosion + dilation (remove small objects)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5)))  # dilation + erosion (remove small holes)
+
+        hsv_frame_filtered = cv2.bitwise_and(frame, frame, mask=mask)  # создание фильтра
+        cv2.imshow('Filtered Web Video', hsv_frame_filtered)
+
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+
+def task4_moments():
+    # захват видео
+    cap = cv2.VideoCapture(0)
+
+    def nothing(args):
+        pass
+
+    # создаем окно для отображения результата и бегунки
+    cv2.namedWindow("setup", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("setup", 300, 200)  # Изменяем размер окна управления
+    cv2.createTrackbar("minH", "setup", 119, 179, nothing)
+    cv2.createTrackbar("minS", "setup", 140, 255, nothing)
+    cv2.createTrackbar("minV", "setup", 3, 255, nothing)
+    cv2.createTrackbar("maxH", "setup", 137, 179, nothing)
+    cv2.createTrackbar("maxS", "setup", 255, 255, nothing)
+    cv2.createTrackbar("maxV", "setup", 255, 255, nothing)
+
+    while True:
+        ret, frame = cap.read()  # Чтение текущего кадра
+        if not ret:  # Проверка на успешное чтение кадра
+            break
+
+        # собираем значения из бегунков в множества
+        minH = cv2.getTrackbarPos('minH', 'setup')
+        minS = cv2.getTrackbarPos('minS', 'setup')
+        minV = cv2.getTrackbarPos('minV', 'setup')
+        maxH = cv2.getTrackbarPos('maxH', 'setup')
+        maxS = cv2.getTrackbarPos('maxS', 'setup')
+        maxV = cv2.getTrackbarPos('maxV', 'setup')
+        min_p = (minH, minS, minV)
+        max_p = (maxH, maxS, maxV)
+
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+        hsv_frame[:, :, 0] = (hsv_frame[:, :, 0] + 128) % 0xFF  # смещение красного цвета в центр
+        # чтобы не искать спектр мы его приведем в нужные значения самостоятельно
+
+        # применяем фильтр, делаем бинаризацию
+        mask = cv2.inRange(hsv_frame, min_p, max_p)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5)))  # erosion + dilation (remove small objects)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5)))  # dilation + erosion (remove small holes)
+
+        hsv_frame_filtered = cv2.bitwise_and(frame, frame, mask=mask)  # применение фильтра
+
+        moments = cv2.moments(mask, True)
+        print(moments['m00'])  # вывод площади объекта
+
+        cv2.imshow('Filtered Web Video', hsv_frame_filtered)
+
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+
+def task5():
+    # захват видео
+    cap = cv2.VideoCapture(0)
+
+    def nothing(args):
+        pass
+
+    # создаем окно для отображения результата и бегунки
+    cv2.namedWindow("setup", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("setup", 300, 200)  # Изменяем размер окна управления
+    cv2.createTrackbar("minH", "setup", 119, 179, nothing)
+    cv2.createTrackbar("minS", "setup", 140, 255, nothing)
+    cv2.createTrackbar("minV", "setup", 3, 255, nothing)
+    cv2.createTrackbar("maxH", "setup", 137, 179, nothing)
+    cv2.createTrackbar("maxS", "setup", 255, 255, nothing)
+    cv2.createTrackbar("maxV", "setup", 255, 255, nothing)
+
+    while True:
+        ret, frame = cap.read()  # Чтение текущего кадра
+        if not ret:  # Проверка на успешное чтение кадра
+            break
+
+        # собираем значения из бегунков в множества
+        minH = cv2.getTrackbarPos('minH', 'setup')
+        minS = cv2.getTrackbarPos('minS', 'setup')
+        minV = cv2.getTrackbarPos('minV', 'setup')
+        maxH = cv2.getTrackbarPos('maxH', 'setup')
+        maxS = cv2.getTrackbarPos('maxS', 'setup')
+        maxV = cv2.getTrackbarPos('maxV', 'setup')
+        min_p = (minH, minS, minV)
+        max_p = (maxH, maxS, maxV)
+
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+        hsv_frame[:, :, 0] = (hsv_frame[:, :, 0] + 128) % 0xFF  # смещение красного цвета в центр
+        # чтобы не искать спектр мы его приведем в нужные значения самостоятельно
+
+        # применяем фильтр, делаем бинаризацию
+        mask = cv2.inRange(hsv_frame, min_p, max_p)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,
+                                np.ones((50, 50)))  # erosion и dilation : удаляем маленькие белые 7объекты
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE,
+                                np.ones((50, 50)))  # dilation и erosion : удаляем маленькие черные пробелы
+
+        hsv_frame_filtered = cv2.bitwise_and(frame, frame, mask=mask)  # применение фильтра
+
+        # Почему изображение (маска) молучилось бинарным, если мы не вызывали соответствующую функцию?
+
+        moments = cv2.moments(mask, True)
+        print(moments['m00'])  # вывод площади объекта
+
+        m01 = moments['m01']  # Y
+        m10 = moments['m10']  # X
+        area = moments['m00']
+        if area > 100:
+            posX = int(m10 / area)
+            posY = int(m01 / area)
+            cv2.circle(frame, (posX, posY), 5, (255, 0, 0), -1)
+
+            x, y, w, h = cv2.boundingRect(mask)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 3)
+
+        cv2.imshow('Filtered Web Video', hsv_frame_filtered)
+        cv2.imshow('Web Video', frame)
+
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+
+#task_1()
+#task2_filtering()
